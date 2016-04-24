1ca5eb582b702a8a584bb1542173def543adaf99
ikikyku <venlawliet@gmail.com>
2016-04-24 13:47:41 +0700

    my first commit on iak

---
+++ app/src/main/java/com/ikikyku/appku/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ikikyku.appku;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+    }
+}
